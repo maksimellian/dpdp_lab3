9cfc5fb8ff7e851277383700dfded67ad23b0794
maksimellian <maksimellian01@yandex.ru>
2021-12-16 18:14:28 +0300

    Scripted auto-commit on change (2021-12-16 18:14:28) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -1,5 +1,5 @@
 import org.apache.spark.SparkConf;
-
+import org.apache.spark.api.java.JavaSparkContext;
 public class AirportApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("dpdp_lab3");
