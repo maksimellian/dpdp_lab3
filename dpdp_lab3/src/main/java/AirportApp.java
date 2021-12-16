aabeb093bfe1ba03212d7118d30bdb5911a33ab0
maksimellian <maksimellian01@yandex.ru>
2021-12-16 18:23:11 +0300

    Scripted auto-commit on change (2021-12-16 18:23:11) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -3,10 +3,12 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 public class AirportApp {
-    private static String AIRPORT_PATH = "L_AIRPORT_ID.csv";
+    private static String AIRPORTS_PATH = "L_AIRPORT_ID.csv";
+    private static String FLIGHTS_PATH = "L_AIRPORT_ID.csv";
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("dpdp_lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> airportRows = sc.textFile(AIRPORT_PATH);
+        JavaRDD<String> airports = sc.textFile(AIRPORTS_PATH);
+        JavaRDD<String> flights = sc.textFile(FLIGHTS_PATH);
     }
 }
