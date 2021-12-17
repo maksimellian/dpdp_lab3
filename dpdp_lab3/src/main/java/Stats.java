5f8f80b7bd397bf8171e2d5065d91da40a8ede78
maksimellian <maksimellian01@yandex.ru>
2021-12-17 15:53:44 +0300

    Scripted auto-commit on change (2021-12-17 15:53:44) by gitwatch.sh

--- dpdp_lab3/src/main/java/Stats.java
+++ dpdp_lab3/src/main/java/Stats.java
@@ -2,4 +2,8 @@ import java.io.Serializable;
 
 public class Stats implements Serializable {
     private double maxDelay;
+    private int lateFlights;
+    private int cancelledFlights;
+    private double lateFlightsPercent;
+    private double cancelledFlightsPercent;
 }
