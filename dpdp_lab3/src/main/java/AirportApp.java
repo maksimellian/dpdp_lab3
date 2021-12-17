4e0b4026e4d093d44458e51e204a58d4d4bd42d5
maksimellian <maksimellian01@yandex.ru>
2021-12-17 15:31:23 +0300

    Scripted auto-commit on change (2021-12-17 15:31:23) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -21,7 +21,7 @@ public class AirportApp {
         airports = removeHeader(airports);
         JavaPairRDD<String, String> airportPairs = airports.mapToPair(row -> {
             String[] rowFields = row.split(COMMA);
-            return new Tuple2<>(removeQuotes(rowFields[]))
+            return new Tuple2<>(removeQuotes(rowFields[AIRPORT_CODE]), removeQuotes(rowFields[AIRPORT_DESCRIPTION]));
         });
 
 
