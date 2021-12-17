7cc82e8e83adbd71f3061ba9bcbf99bec8e6e977
maksimellian <maksimellian01@yandex.ru>
2021-12-17 15:23:58 +0300

    Scripted auto-commit on change (2021-12-17 15:23:58) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -6,12 +6,28 @@ import scala.Tuple2;
 public class AirportApp {
     private static String AIRPORTS_PATH = "L_AIRPORT_ID.csv";
     private static String FLIGHTS_PATH = "L_AIRPORT_ID.csv";
-    private static 
+    private static String QUOTE = "\"";
+    private static int ORIGIN_AIRPORT_ID = 11;
+    private static int DEST_AIRPORT_ID = 14;
+    public static final int DELAY = 18;
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("dpdp_lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airports = sc.textFile(AIRPORTS_PATH);
+        airports = removeHeader(airports);
+
+
+
         JavaRDD<String> flights = sc.textFile(FLIGHTS_PATH);
-        JavaPairRDD<String, String> airportData = airports.mapToPair();
+    }
+
+    public static String deleteQuotes(String str) {
+        return str.replaceAll(QUOTE, "");
+    }
+
+    public static JavaRDD<String> removeHeader(JavaRDD<String> rows) {
+        String rowsHeader = rows.first();
+        return rows.filter(row -> !row.equals(rowsHeader));
     }
 }
