c98cafcc1cf4edb913b53e2f90da8f42fa645f83
maksimellian <maksimellian01@yandex.ru>
2021-12-17 15:37:40 +0300

    Scripted auto-commit on change (2021-12-17 15:37:40) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -27,7 +27,7 @@ public class AirportApp {
             String[] rowFields = row.split(COMMA);
             return new Tuple2<>(removeQuotes(rowFields[AIRPORT_CODE]), removeQuotes(rowFields[AIRPORT_DESCRIPTION]));
         });
-        final Broadcast<Map<String, >>
+        final Broadcast<Map<String, String>> airportsBroadcasted
 
         JavaRDD<String> flights = sc.textFile(FLIGHTS_PATH);
     }
