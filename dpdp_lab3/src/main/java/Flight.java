addb382ebff68564fbe065b4c64c1cd7ff63f8a6
maksimellian <maksimellian01@yandex.ru>
2021-12-17 16:23:20 +0300

    Scripted auto-commit on change (2021-12-17 16:23:20) by gitwatch.sh

--- dpdp_lab3/src/main/java/Flight.java
+++ dpdp_lab3/src/main/java/Flight.java
@@ -4,13 +4,16 @@ public class Flight implements Serializable {
     private int originAirportId;
     private int destinationAirportId;
     private double delay;
-    private double cancellation;
+    private double cancellationStatus; // if cancelled 1.00, 0.00 else
 
     public Flight() {}
 
-    public Flight(int originAirportId, int destinationAirportId, double delay) {
+    public Flight(int originAirportId, int destinationAirportId, double delay, double cancellationStatus) {
         this.originAirportId = originAirportId;
         this.destinationAirportId = destinationAirportId;
         this.delay = delay;
+        this.cancellationStatus = cancellationStatus;
     }
+
+    
 }
