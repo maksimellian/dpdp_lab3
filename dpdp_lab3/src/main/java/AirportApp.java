01658043086937f9ff7b3742701991a73c44dba3
maksimellian <maksimellian01@yandex.ru>
2021-12-18 03:42:04 +0300

    Scripted auto-commit on change (2021-12-18 03:42:04) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -32,7 +32,7 @@ public class AirportApp {
 
         JavaRDD<String> flights = sc.textFile(FLIGHTS_PATH);
         flights = removeHeader(flights);
-        
+        JavaPairRDD<Tuple2, >
     }
 
     public static String removeQuotes(String str) {
