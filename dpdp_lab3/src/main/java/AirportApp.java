982e458cc60899bab8f9df49e6a986b2279a87c6
maksimellian <maksimellian01@yandex.ru>
2021-12-18 03:54:38 +0300

    Scripted auto-commit on change (2021-12-18 03:54:38) by gitwatch.sh

--- dpdp_lab3/src/main/java/AirportApp.java
+++ dpdp_lab3/src/main/java/AirportApp.java
@@ -39,7 +39,7 @@ public class AirportApp {
             return new Tuple2<>(new Tuple2(fields[ORIGIN_AIRPORT_ID], fields[DEST_AIRPORT_ID]),
                     new Flight(Integer.parseInt(fields[ORIGIN_AIRPORT_ID]), Integer.parseInt(fields[DEST_AIRPORT_ID]),
                             delay, cancellationCode));
-        }).combineByKey();
+        }).combineByKey(ST);
     }
 
     public static String removeQuotes(String str) {
