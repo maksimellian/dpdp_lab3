f6233d43d1e6c2cceeb67ef98401b6bc124f7694
maksimellian <maksimellian01@yandex.ru>
2022-01-10 23:21:30 +0300

    Scripted auto-commit on change (2022-01-10 23:21:30) by gitwatch.sh

--- dpdp_lab3/src/main/java/MergeValue.java
+++ dpdp_lab3/src/main/java/MergeValue.java
@@ -1,2 +1,8 @@
-public class MergeValue {
+import org.apache.spark.api.java.function.Function2;
+
+public class MergeValue implements Function2<Stats, String, Stats> {
+    @Override
+    public Stats call(Stats stats, String delay) throws Exception {
+        return stats.mergeValue(delay);
+    }
 }
