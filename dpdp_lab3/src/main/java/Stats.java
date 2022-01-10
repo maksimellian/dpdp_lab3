f8183b050eda5a16b851d31bc1c69e91c506e27a
maksimellian <maksimellian01@yandex.ru>
2022-01-10 23:41:08 +0300

    Scripted auto-commit on change (2022-01-10 23:41:08) by gitwatch.sh

--- dpdp_lab3/src/main/java/Stats.java
+++ dpdp_lab3/src/main/java/Stats.java
@@ -57,7 +57,7 @@ public class Stats implements Serializable {
         if (this.totalFlights > 0) {
             double lateFlightsPercent = ((float) lateFlights / (float) totalFlights) * PERCENT;
             String formattedLateFlightsPercent = String.format(FLOAT_INPUT_FORMAT, lateFlightsPercent);
-            double cancelledFlightsPercent = cancelledFlights / totalFlights * PERCENT;
+            double cancelledFlightsPercent = ((float) cancelledFlights /(float) totalFlights) * PERCENT;
             String formattedCancelledFlightsPercent = String.format(FLOAT_INPUT_FORMAT, cancelledFlightsPercent);
             return "maxDelay = " + this.maxDelay + " lateFlightsPercent = " + lateFlightsPercent
                     + " cancelledFlightsPercent = " + cancelledFlightsPercent;
