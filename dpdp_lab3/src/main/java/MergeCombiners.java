78bfcf68a6e13daae3b1b9bc469aecfd135f2147
maksimellian <maksimellian01@yandex.ru>
2022-01-10 23:19:20 +0300

    Scripted auto-commit on change (2022-01-10 23:19:20) by gitwatch.sh

--- dpdp_lab3/src/main/java/MergeCombiners.java
+++ dpdp_lab3/src/main/java/MergeCombiners.java
@@ -1,4 +1,8 @@
-import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 
-public class MergeCombiners implements Function<> {
+public class MergeCombiners implements Function2<Stats, Stats, Stats> {
+    @Override
+    public Stats call(Stats stats, Stats stats2) throws Exception {
+        return null;
+    }
 }
