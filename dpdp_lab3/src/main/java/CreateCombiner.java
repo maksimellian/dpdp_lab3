921aae8afeb5780ed97ea4887e4f6df94d0e2851
maksimellian <maksimellian01@yandex.ru>
2022-01-10 22:39:30 +0300

    Scripted auto-commit on change (2022-01-10 22:39:30) by gitwatch.sh

--- dpdp_lab3/src/main/java/CreateCombiner.java
+++ dpdp_lab3/src/main/java/CreateCombiner.java
@@ -1,2 +1,8 @@
-public class CreateCombiner {
+import org.apache.spark.api.java.function.Function;
+
+public class CreateCombiner implements Function<String, Flight> {
+    @Override
+    public Flight call(String s) throws Exception {
+        return Flight.;
+    }
 }
