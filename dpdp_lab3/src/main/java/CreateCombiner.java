0f3cdbdcb1b0be181cdc4c056719fd39bea29d4a
maksimellian <maksimellian01@yandex.ru>
2022-01-10 23:15:18 +0300

    Scripted auto-commit on change (2022-01-10 23:15:18) by gitwatch.sh

--- dpdp_lab3/src/main/java/CreateCombiner.java
+++ dpdp_lab3/src/main/java/CreateCombiner.java
@@ -1,2 +1,8 @@
-public class CreateCombiner {
+import java.util.function.Function;
+
+public class CreateCombiner implements Function<String, Stats> {
+    @Override
+    public Stats apply(String s) {
+        return null;
+    }
 }
